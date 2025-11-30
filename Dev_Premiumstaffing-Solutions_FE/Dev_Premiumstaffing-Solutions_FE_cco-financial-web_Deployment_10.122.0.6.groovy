29a9c9d984258577eb894f3b28cd3aa334f8ae2b
RvSingh013 <126268789+ravinderpalflynaut@users.noreply.github.com>
2025-11-30 22:51:01 +0530

    Update Dev_Premiumstaffing-Solutions_FE_cco-financial-web_Deployment_10.122.0.6.groovy

--- Dev_Premiumstaffing-Solutions_FE/Dev_Premiumstaffing-Solutions_FE_cco-financial-web_Deployment_10.122.0.6.groovy
+++ Dev_Premiumstaffing-Solutions_FE/Dev_Premiumstaffing-Solutions_FE_cco-financial-web_Deployment_10.122.0.6.groovy
@@ -3,9 +3,13 @@ pipeline {
         label 'dev-rvsingh'
     }
  environment {
-        GIT_CREDENTIALS_ID = 'New-Github-Jenkins-Cred-2025'
-        GIT_REPO = 'https://github.com/FlyNaut-Dev/cco-financial-web.git'
-        GIT_BRANCH = 'Development'
+  GIT_CREDENTIALS_ID = 'New-Github-Jenkins-Cred-2025'
+  GIT_REPO = 'https://github.com/FlyNaut-Dev/cco-financial-web.git'
+  GIT_BRANCH = 'Development'
+  DOCKER_HUB_REPO = 'hub.flynautstaging.com/cco-dev/test'
+  DOCKER_IMAGE_TAG = 'latest'
+  DOCKER_REGISTRY_URL = 'https://hub.finvasia.com'
+  DOCKER_CREDENTIALS_ID = 'DockerHub'
     }
     stages {
         stage('Clone Repository') {
@@ -16,4 +20,26 @@ pipeline {
             }
         }
     }
+
+    stage('Build Docker Image') {
+         steps {
+             script {
+                def image = docker.build("${DOCKER_HUB_REPO}:${DOCKER_IMAGE_TAG}")
+             }
+          }
+      }
+
+         stage('Push Docker Image to Docker Registry') {
+            steps {
+                script {
+                    docker.withRegistry("${DOCKER_REGISTRY_URL}", "${DOCKER_CREDENTIALS_ID}") {
+                        def image = docker.image("${DOCKER_HUB_REPO}:${DOCKER_IMAGE_TAG}")
+                        image.push()
+                    }
+                }
+            }
+        }
+
+
+ 
 }
