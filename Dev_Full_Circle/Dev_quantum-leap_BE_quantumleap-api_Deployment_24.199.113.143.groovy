cad78e0514f018f2f59e06b227b843b5b93453d6
RvSingh013 <126268789+ravinderpalflynaut@users.noreply.github.com>
2026-01-07 02:08:47 +0530

    Update Dev_quantum-leap_BE_quantumleap-api_Deployment_24.199.113.143.groovy

--- Dev_Full_Circle/Dev_quantum-leap_BE_quantumleap-api_Deployment_24.199.113.143.groovy
+++ Dev_Full_Circle/Dev_quantum-leap_BE_quantumleap-api_Deployment_24.199.113.143.groovy
@@ -38,23 +38,64 @@ pipeline {
             }
         }
 
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    docker.build("${DOCKER_HUB_REPO}:${DOCKER_IMAGE_TAG}")
+                }
+            }
+        }
+
+        stage('Push Docker Image') {
+            steps {
+                script {
+                    docker.withRegistry("https://${DOCKER_REGISTRY_URL}", "${DOCKER_CREDENTIALS_ID}") {
+                        docker.image("${DOCKER_HUB_REPO}:${DOCKER_IMAGE_TAG}").push()
+                       // docker.image("${DOCKER_HUB_REPO}:${DOCKER_IMAGE_TAG}").push("${BUILD_ID}")
+                    }
+                }
+            }
+        }
+
+        stage('Cleanup Local Docker Images') {
+            steps {
+                sh 'docker image prune -f'
+            }
+        }
 
         stage('Docker Login + Deploy on Remote') {
             steps {
                 sshagent(['Jenkins-Deployment']) {
+                    withCredentials([
+                        usernamePassword(
+                            credentialsId: 'DockerHub',
+                            usernameVariable: 'REG_USER',
+                            passwordVariable: 'REG_PASS'
+                        )
+                    ]) {
                     sh '''
                         ssh -o StrictHostKeyChecking=no -p ${DEPLOY_PORT} ${DEPLOY_USER}@${DEPLOY_SERVER} "
-                       cd  ${COMPOSE_PATH} && mkdir -p ravinderpalsinghdheri-2026
-                      #  echo '${REG_PASS}' | docker login ${DOCKER_REGISTRY_URL} -u '${REG_USER}' --password-stdin &&
-                        # docker compose -f ${COMPOSE_PATH}/docker-compose.yml pull ${REPONAME} &&
-                        # docker compose -f ${COMPOSE_PATH}/docker-compose.yml up -d ${REPONAME}
+                        echo '${REG_PASS}' | docker login ${DOCKER_REGISTRY_URL} -u '${REG_USER}' --password-stdin &&
+                        docker compose -f ${COMPOSE_PATH}/docker-compose.yml pull ${REPONAME} &&
+                        docker compose -f ${COMPOSE_PATH}/docker-compose.yml up -d ${REPONAME}
                         "
                     '''
-
+                    }
                 }
             }
         }
-         
+
+        // stage('Remove Unused & Dangling Images on Remote Server') {
+        //     steps {
+        //         sshagent(['Jenkins-Deployment']) {
+        //             script {
+        //                  sh '''
+        //                   ssh -o StrictHostKeyChecking=no -p ${DEPLOY_PORT} ${DEPLOY_USER}@${DEPLOY_SERVER} "docker system prune -a -f"
+        //                  '''
+        //             }
+        //         }
+        //     }
+        // }           
         
     }
 }
