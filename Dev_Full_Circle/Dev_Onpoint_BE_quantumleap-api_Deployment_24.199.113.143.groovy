e5d1bf798e33c0f12e6819302603e2df3ccffa24
RvSingh013 <126268789+ravinderpalflynaut@users.noreply.github.com>
2026-01-07 00:14:06 +0530

    Update and rename Dev_Onpoint_BE_onpoint-backend_Deployment_24.199.113.143.groovy to Dev_Onpoint_BE_quantumleap-api_Deployment_24.199.113.143.groovy

--- Dev_Full_Circle/Dev_Onpoint_BE_quantumleap-api_Deployment_24.199.113.143.groovy
+++ Dev_Full_Circle/Dev_Onpoint_BE_quantumleap-api_Deployment_24.199.113.143.groovy
@@ -4,17 +4,17 @@ pipeline {
     }
 
     environment {
-        REPONAME              = 'onpoint-backend'
+        REPONAME              = 'quantumleap-api'
         GIT_CREDENTIALS_ID    = 'Jenkins-Git-Cred'
-        GIT_REPO              = "https://github.com/FlyNaut-Dev/onpoint-backend.git"
+        GIT_REPO              = "https://github.com/FlyNaut-Dev/quantumleap-api.git"
         GIT_BRANCH            = 'development'
 
-        DOCKER_HUB_REPO       = 'hub.flynautstaging.com/onpoint-dev/onpoint-backend'
+        DOCKER_HUB_REPO       = 'hub.flynautstaging.com/quantumleap-dev/quantumleap-api'
         DOCKER_IMAGE_TAG      = 'latest'
         DOCKER_REGISTRY_URL   = 'hub.flynautstaging.com'
         DOCKER_CREDENTIALS_ID = 'DockerHub'
 
-        DEPLOY_SERVER         =  '10.122.0.9'
+        DEPLOY_SERVER         =  '24.199.113.143'
         DEPLOY_USER           = 'root'
         DEPLOY_PORT           = '22'
         COMPOSE_PATH          = '/var/www/dev-project'
