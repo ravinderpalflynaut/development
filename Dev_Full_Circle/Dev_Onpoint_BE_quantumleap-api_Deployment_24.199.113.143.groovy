98959d5f4b6c5887473af5dd5f23bd9cdf186e31
RvSingh013 <126268789+ravinderpalflynaut@users.noreply.github.com>
2026-01-07 01:08:50 +0530

    Update Dev_Onpoint_BE_quantumleap-api_Deployment_24.199.113.143.groovy

--- Dev_Full_Circle/Dev_Onpoint_BE_quantumleap-api_Deployment_24.199.113.143.groovy
+++ Dev_Full_Circle/Dev_Onpoint_BE_quantumleap-api_Deployment_24.199.113.143.groovy
@@ -66,21 +66,21 @@ pipeline {
         stage('Docker Login + Deploy on Remote') {
             steps {
                 sshagent(['Development-Cred']) {
-                    withCredentials([
-                        usernamePassword(
-                            credentialsId: 'DockerHub',
-                            usernameVariable: 'REG_USER',
-                            passwordVariable: 'REG_PASS'
-                        )
-                    ]) {
+                    // withCredentials([
+                    //     usernamePassword(
+                    //         credentialsId: 'DockerHub',
+                    //         usernameVariable: 'REG_USER',
+                    //         passwordVariable: 'REG_PASS'
+                    //     )
+                    // ]) {
                     sh '''
                         ssh -o StrictHostKeyChecking=no -p ${DEPLOY_PORT} ${DEPLOY_USER}@${DEPLOY_SERVER} "
-                        echo '${REG_PASS}' | docker login ${DOCKER_REGISTRY_URL} -u '${REG_USER}' --password-stdin &&
+                      //  echo '${REG_PASS}' | docker login ${DOCKER_REGISTRY_URL} -u '${REG_USER}' --password-stdin &&
                         docker compose -f ${COMPOSE_PATH}/docker-compose.yml pull ${REPONAME} &&
                         docker compose -f ${COMPOSE_PATH}/docker-compose.yml up -d ${REPONAME}
                         "
                     '''
-                    }
+                    // }
                 }
             }
         }
