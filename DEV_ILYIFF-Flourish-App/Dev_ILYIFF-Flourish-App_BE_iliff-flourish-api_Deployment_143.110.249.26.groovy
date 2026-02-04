2203088ca12bc91c509584ecf6fb68afc0566da9
RvSingh013 <126268789+ravinderpalflynaut@users.noreply.github.com>
2026-02-04 23:55:39 +0530

    Update Dev_ILYIFF-Flourish-App_BE_iliff-flourish-api_Deployment_143.110.249.26.groovy

--- DEV_ILYIFF-Flourish-App/Dev_ILYIFF-Flourish-App_BE_iliff-flourish-api_Deployment_143.110.249.26.groovy
+++ DEV_ILYIFF-Flourish-App/Dev_ILYIFF-Flourish-App_BE_iliff-flourish-api_Deployment_143.110.249.26.groovy
@@ -4,12 +4,12 @@ pipeline {
     }
 
     environment {
-        REPONAME              = 'full-circle-api'
+        REPONAME              = 'iliff-flourish-api'
         GIT_CREDENTIALS_ID    = 'Jenkins-Git-Cred'
-        GIT_REPO              = "https://github.com/FlyNaut-Dev/full-circle-api.git"
+        GIT_REPO              = "https://github.com/FlyNaut-Dev/iliff-flourish-api.git"
         GIT_BRANCH            = 'development'
 
-        DOCKER_HUB_REPO       = 'hub.flynautstaging.com/fullcircle-dev/full-circle-api'
+        DOCKER_HUB_REPO       = 'hub.flynautstaging.com/ilyiff-flourish-app-dev/iliff-flourish-api'
         DOCKER_IMAGE_TAG      = 'latest'
         DOCKER_REGISTRY_URL   = 'hub.flynautstaging.com'
         DOCKER_CREDENTIALS_ID = 'DockerHub'
