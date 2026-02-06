25392377b4344dd76d825377c20af5163eec54fc
RvSingh013 <126268789+ravinderpalflynaut@users.noreply.github.com>
2026-02-07 00:13:40 +0530

    Update Dev_CCO-Retail_FE_cco-ai-sales-app-web_Deployment_104.236.123.23.groovy

--- DEV_CCO-Ai-Sales-App/Dev_CCO-Retail_FE_cco-ai-sales-app-web_Deployment_104.236.123.23.groovy
+++ DEV_CCO-Ai-Sales-App/Dev_CCO-Retail_FE_cco-ai-sales-app-web_Deployment_104.236.123.23.groovy
@@ -58,7 +58,8 @@ pipeline {
 
         stage('Cleanup Local Docker Images') {
             steps {
-                sh 'docker system  prune -a -f'
+                sh "docker system  prune -a -f"
+                sh "docker rmi $(docker images -f "dangling=true" -q)"
             }
         }
 
@@ -90,7 +91,7 @@ pipeline {
                     script {
                          sh '''
                           ssh -o StrictHostKeyChecking=no -p ${DEPLOY_PORT} ${DEPLOY_USER}@${DEPLOY_SERVER} "docker system prune -a -f"
-                           // ssh -o StrictHostKeyChecking=no -p ${DEPLOY_PORT} ${DEPLOY_USER}@${DEPLOY_SERVER} "docker rmi $(docker images -f "dangling=true" -q)"
+                          ssh -o StrictHostKeyChecking=no -p ${DEPLOY_PORT} ${DEPLOY_USER}@${DEPLOY_SERVER} "docker rmi $(docker images -f "dangling=true" -q)"
                          '''
                     }
                 }
